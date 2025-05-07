14107eb57a70d3e5242fa23fc392f1d4614ec9c3
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2025-05-07 10:31:20 +0700

    Up

---
+++ main.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from PIL import Image
+from io import BytesIO
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load model
+@st.cache_resource
+def load_model():
+    model = models.resnet50(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)
+    model.to(device)
+
+    checkpoint = torch.load("./model/checkpoint_epoch_10.pt", map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+
+    return model
+
+model = load_model()
+
+def RandomImagePrediction(uploaded_file):
+    try:
+        img_array = Image.open(uploaded_file).convert("RGB")
+    except Exception as e:
+        return f"Lỗi khi đọc ảnh: {e}"
+
+    data_transforms = transforms.Compose([
+        transforms.Resize((224, 224)), 
+        transforms.ToTensor(), 
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ])
+    img = data_transforms(img_array).unsqueeze(0)
+    load = DataLoader(img)
+
+    for x in load:
+        x = x.to(device)
+        pred = model(x)
+        _, preds = torch.max(pred, 1)
+        if preds[0] == 1:
+            return "🐶 Chó"
+        else:
+            return "🐱 Mèo"
+    return "Lỗi không xác định"
+
+
+# Giao diện Streamlit
+st.title("📸 Phân loại Chó hoặc Mèo")
+uploaded_file = st.file_uploader("Tải lên ảnh (jpg, jpeg, png)", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Ảnh đã tải lên", use_column_width=True)
+
+    # Đưa lại uploaded_file vào hàm
+    uploaded_file.seek(0)  # Reset pointer trước khi đọc lại
+    prediction = RandomImagePrediction(uploaded_file)
+    st.markdown(f"### 👉 Dự đoán: **{prediction}**")
